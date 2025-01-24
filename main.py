db5b57121d401c2777a05fda8af30fd96a47e7f9
mike <mike.tomasulo@gmail.com>
2025-01-24 11:36:06 -0500

    webserver

--- main.py
+++ main.py
@@ -1,14 +1,40 @@
 import RPi.GPIO as GPIO
 import time
-GPIO.setmode (GPIO.BCM)
-pinlist = [14, 15]
-for pin in pinlist:
-    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+from http.server import BaseHTTPRequestHandler, HTTPServer
 
-x=0
-while x < 300:
+hostname = "pitester"
+serverport = 80
+class TestServer(BaseHTTPRequestHandler):
+    def do_GET(self):
+        self.send_response(200)
+        self.send_header("Content-type", "text/html")
+        self.end_headers()
+        self.wfile.write(bytes("<html><head><title>https://pythonbasics.org</title></head>", "utf-8"))
+        self.wfile.write(bytes("<p>Request: %s</p>" % self.path, "utf-8"))
+        self.wfile.write(bytes("<body>", "utf-8"))
+        self.wfile.write(bytes("<p>This is an example web server.</p>", "utf-8"))
+        self.wfile.write(bytes("</body></html>", "utf-8"))
+
+if __name__ == "__main__":
+    webServer = HTTPServer((hostname, serverport), TestServer)
+    print("Server started http://%s:%s" % (hostname, serverport))
+    try:
+        webServer.serve_forever()
+    except KeyboardInterrupt:
+        pass
+
+    webServer.server_close()
+    print("Server stopped.")
+    
+    GPIO.setmode (GPIO.BCM)
+    pinlist = [14, 15]
     for pin in pinlist:
-        print(f"pin {pin}")
-        print(GPIO.input(pin))
-    x+=1
-    time.sleep(.5)
+        GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+
+    x=0
+    while x < 300:
+        for pin in pinlist:
+            print(f"pin {pin}")
+            print(GPIO.input(pin))
+        x+=1
+        time.sleep(.5)
