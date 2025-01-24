456e2655bee1ef4fcc79de178ca524faed5b3f2f
mike <mike.tomasulo@gmail.com>
2025-01-24 11:31:06 -0500

    pin list

--- main.py
+++ main.py
@@ -1,11 +1,14 @@
 import RPi.GPIO as GPIO
 import time
 GPIO.setmode (GPIO.BCM)
-GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_UP)
+pinlist = [14, 15]
+for pin in pinlist:
+    GPIO.setup(pin, GPIO.IN, pull_up_down=GPIO.PUD_UP)
 
 x=0
 while x < 300:
-    print("test")
-    print(GPIO.input(14))
+    for pin in pinlist:
+        print(f"pin {pin}")
+        print(GPIO.input(pin))
     x+=1
     time.sleep(.5)
